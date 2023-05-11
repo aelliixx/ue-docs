0c78ddadeea6823947994e5f3c898a99ab54877d
Donathan <mockus49@gmail.com>
2023-05-11 17:45:30 +0200

    Specifiers: fix anchor should be in h2, not h2 in anchor

--- components/mdx/Specifier.tsx
+++ components/mdx/Specifier.tsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 
 function Specifier({type, name, meta}: { type: string, name: string, meta?: boolean }) {
     return (
-        <a href={`#${name}`}><h2 className={styles.specifier} id={name}>
+        <h3 className={styles.specifier} id={name}><a href={`#${name}`}>
             <span className={styles.span_begin}>
                 {type}({meta && "meta = "}
             </span>
             {name}
             <span className={styles.span_end}>)</span>
-        </h2>
         </a>
+        </h3>
     );
 }
 
